504e9f07fba2234debae402da13e08fa2a8c64b7
2000923 <2000923@unmsm.edu.pe>
2024-01-05 16:49:05 -0500

    new changes in the project

--- bash/script_rhel8_rhcsa.sh
+++ bash/script_rhel8_rhcsa.sh
@@ -16,9 +16,9 @@
 
 #!/bin/sh
 ## Loading Variables
-IP_ADDR=192.168.20.10
-HOSTNAME=rhel8beta.labrhel.com
-SHORTNAME=rhel8beta
+IP_ADDR=192.168.20.50
+HOSTNAME=rhel8master.labrhel.com
+SHORTNAME=rhel8master
 DOMAIN=labrhel.com
 REALM=LABRHEL.COM
 LDAPHOME=/home/ldap
@@ -37,6 +37,8 @@ yum install -y ipa-server ipa-server-dns bind-dyndb-ldap
 
 ## Setting the right config on hosts file
 echo "$IP_ADDR $HOSTNAME $SHORTNAME" >> /etc/hosts
+
+sysctl -w net.ipv6.conf.lo.disable_ipv6=0 ; sysctl -p
 
 # Installing everything unattended
 ipa-server-install --domain=$DOMAIN --realm=$REALM --ds-password=password --admin-password=password --hostname=$HOSTNAME --ip-address=$IP_ADDR --reverse-zone=20.168.192.in-addr.arpa. --forwarder=8.8.8.8 --allow-zone-overlap --setup-dns --unattended
@@ -187,23 +189,23 @@ systemctl restart nmb
 # nmcli connection up eth0
 
 # Creating Network repo
-mkdir -p /var/www/html/repos
+#mkdir -p /var/www/html/repos
 #cp -a /mnt/iso /var/ftp/pub/repos/rhel8
-cp -a /mnt /var/www/html/repos/rhel8
-restorecon -Rvv /var/www/html
-
+#cp -a /mnt /var/www/html/repos/rhel8
+#restorecon -Rvv /var/www/html
+#
 # Creating Base Repo File - base
 ## BaseOS and AppStream:
-echo "[base]" >> /var/www/html/repos/base.repo
-echo "name = Base Reporitory for RHEL 8" >> /var/www/html/repos/base.repo
-echo "baseurl = http://SERVER_NAME.labrhel.com/repos/rhel8/BaseOS" >> /var/www/html/repos/base.repo
-echo "enabled = 0" >> /var/www/html/repos/base.repo
-echo "gpgcheck = 0" >> /var/www/html/repos/base.repo
-
-echo "[appstream]" >> /var/www/html/repos/base.repo
-echo "name = App Stream Reporitory for RHEL 8" >> /var/www/html/repos/base.repo
-echo "baseurl = http://SERVER_NAME.labrhel.com/repos/rhel8/AppStream" >> /var/www/html/repos/base.repo
-echo "enabled = 0" >> /var/www/html/repos/base.repo
-echo "gpgcheck = 0" >> /var/www/html/repos/base.repo
+#echo "[base]" >> /var/www/html/repos/base.repo
+#echo "name = Base Reporitory for RHEL 8" >> /var/www/html/repos/base.repo
+#echo "baseurl = http://rhel8master.labrhel.com/repos/rhel8/BaseOS" >> /var/www/html/repos/base.repo
+#echo "enabled = 0" >> /var/www/html/repos/base.repo
+#echo "gpgcheck = 0" >> /var/www/html/repos/base.repo
+#
+#echo "[appstream]" >> /var/www/html/repos/base.repo
+#echo "name = App Stream Reporitory for RHEL 8" >> /var/www/html/repos/base.repo
+#echo "baseurl = http://rhel8master.labrhel.com/repos/rhel8/AppStream" >> /var/www/html/repos/base.repo
+#echo "enabled = 0" >> /var/www/html/repos/base.repo
+#echo "gpgcheck = 0" >> /var/www/html/repos/base.repo
 
 # systemctl restart vsftpd
